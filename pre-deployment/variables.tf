c83eeef52af6041df976a065b5f6125cb75c1b5b
Thiago Murilo Diniz <thiagodiniz.info@gmail.com>
2025-07-25 00:45:19 -0400

    adds dynamodb

--- pre-deployment/variables.tf
+++ pre-deployment/variables.tf
@@ -16,3 +16,19 @@ variable "active_states" {
     sa-east-1 = string
   })
 }
+
+variable "dynamodb_idempotency" {
+  type = object({
+    name                   = string
+    billing_mode           = string
+    point_in_time_recovery = bool
+
+    read_min                 = number
+    read_max                 = number
+    read_autoscale_threshold = number
+
+    write_min                 = number
+    write_max                 = number
+    write_autoscale_threshold = number
+  })
+}
